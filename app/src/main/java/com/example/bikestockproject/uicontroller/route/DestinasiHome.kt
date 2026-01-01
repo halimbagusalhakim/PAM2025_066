017f8f96f94d05f920171dc83a3d69801dd9d28a
halimbagusalhakim <halsat132@gmail.com>
2026-01-01 19:59:37 +0700

    memebuat route untuk destinasihome

---
+++ app/src/main/java/com/example/bikestockproject/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.bikestockproject.uicontroller.route
+
+import com.example.bikestockproject.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.home
+}
