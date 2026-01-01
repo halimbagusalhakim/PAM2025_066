b72a9d9533ac1010d4bbc08d1980efa52597d37b
halimbagusalhakim <halsat132@gmail.com>
2026-01-01 20:00:41 +0700

    memebuat nama route untuk destinasiProduk

---
+++ app/src/main/java/com/example/bikestockproject/uicontroller/route/DestinasiProduk.kt
@@ -0,0 +1,35 @@
+package com.example.bikestockproject.uicontroller.route
+
+import com.example.bikestockproject.R
+
+
+object DestinasiProdukList : DestinasiNavigasi {
+    override val route = "list_produk"
+    override val titleRes = R.string.list_produk
+}
+
+object DestinasiProdukEntry : DestinasiNavigasi {
+    override val route = "entry_produk"
+    override val titleRes = R.string.tambah_produk
+}
+
+object DestinasiProdukDetail : DestinasiNavigasi {
+    override val route = "detail_produk"
+    override val titleRes = R.string.detail_produk
+    const val produkIdArg = "produkId"
+    val routeWithArgs = "$route/{$produkIdArg}"
+}
+
+object DestinasiProdukEdit : DestinasiNavigasi {
+    override val route = "edit_produk"
+    override val titleRes = R.string.edit_produk
+    const val produkIdArg = "produkId"
+    val routeWithArgs = "$route/{$produkIdArg}"
+}
+
+object DestinasiProdukStok : DestinasiNavigasi {
+    override val route = "stok_produk"
+    override val titleRes = R.string.update_stok
+    const val produkIdArg = "produkId"
+    val routeWithArgs = "$route/{$produkIdArg}"
+}
