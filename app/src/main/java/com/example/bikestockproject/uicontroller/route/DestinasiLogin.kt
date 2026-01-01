d965b599bd94c7045d7dadba133ad9fc7432cc27
halimbagusalhakim <halsat132@gmail.com>
2026-01-01 19:59:45 +0700

    memebuat route untuk login

---
+++ app/src/main/java/com/example/bikestockproject/uicontroller/route/DestinasiLogin.kt
@@ -0,0 +1,8 @@
+package com.example.bikestockproject.uicontroller.route
+
+import com.example.bikestockproject.R
+
+object DestinasiLogin : DestinasiNavigasi {
+    override val route = "login"
+    override val titleRes = R.string.login
+}
